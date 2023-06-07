86e299f65e19598f4ce7b66d1539f12de1814b88
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-07 19:46:57 +0600

    Add a top app bar

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -2,15 +2,30 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrProductGridFragmentBinding
 
 class ProductGridFragment : Fragment() {
-
+private lateinit var binding: ShrProductGridFragmentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        binding = ShrProductGridFragmentBinding.inflate(inflater)
+        (activity as AppCompatActivity).setSupportActionBar(binding.appBar)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
