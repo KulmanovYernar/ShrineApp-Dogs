60630f8f62ee7480bafaf4b47fbdf4cd9a76476f
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-07 18:37:18 +0600

    Navigate to ProductGridFragment

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,25 +1,52 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrLoginFragmentBinding
 
 /**
  * Fragment representing the login screen for Shrine.
  */
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(){
 
+    private lateinit var binding:ShrLoginFragmentBinding
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
         // Snippet from "Navigate to the next Fragment" section goes here.
+        binding = ShrLoginFragmentBinding.inflate(layoutInflater)
+        val view = binding.root
 
-        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+        binding.nextButton.setOnClickListener{
+            if(!isPasswordValid(binding.passwordEditText.text!!)){
+                binding.passwordTextInput.error = getString(R.string.shr_error_password)
+            }
+            else{
+                binding.passwordTextInput.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+
+        }
+
+        binding.passwordEditText.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(binding.passwordEditText.text!!)) {
+                binding.passwordTextInput.error = null
+            }
+            false
+
+        }
+
         return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+        private fun isPasswordValid(text:Editable?):Boolean{
+            return text != null && text.length >= 8
+
+        }
 }
